f175a4d68ba2dcffe0eec1fa45a7deee9f818bbb
Your Name <you@example.com>
2023-04-07 17:13:37 +0700

    kfc group 7

--- js/firebase.js
+++ js/firebase.js
@@ -7,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5H2kuLhe296lu-qnlVlT_UGsREXHQoNU",
-  authDomain: "thietkewebkfc.firebaseapp.com",
-  databaseURL: "https://thietkewebkfc-default-rtdb.firebaseio.com",
-  projectId: "thietkewebkfc",
-  storageBucket: "thietkewebkfc.appspot.com",
-  messagingSenderId: "1060620707906",
-  appId: "1:1060620707906:web:9050bcc4580cdf3d7925db",
-  measurementId: "G-RQSJDE5VTM"
+  apiKey: "AIzaSyBPiQKPERrHqkhkylaKEjIXdJxbFdaqSNE",
+  authDomain: "kfcwebsite-8e758.firebaseapp.com",
+  projectId: "kfcwebsite-8e758",
+  storageBucket: "kfcwebsite-8e758.appspot.com",
+  messagingSenderId: "793862283616",
+  appId: "1:793862283616:web:76d09fa8e8ae0099277e05",
+  measurementId: "G-ET73YXJDGC"
 };
 
 // Initialize Firebase
